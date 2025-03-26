13902a8c262e27a33d570c61c02e1f396624e1fc
xZero707 <aleksandar@puharic.com>
2025-03-26 22:23:26 +0100

    Update to use snake_case

--- src/setup-unit.sh
+++ src/setup-unit.sh
@@ -3,25 +3,25 @@
 set -eux
 
 main() {
-    local unitOpt="/opt/unit/"
+    local unit_opt="/opt/unit/"
 
     mkdir /rootfs-build/var/lib/unit/state/certs -p
 
-    if [ -d "${unitOpt}/usr/" ]; then
-        rsync -ah --progress "${unitOpt}/usr" /rootfs-build/
-        rm -rf "${unitOpt}"
+    if [ -d "${unit_opt}/usr/" ]; then
+        rsync -ah --progress "${unit_opt}/usr" /rootfs-build/
+        rm -rf "${unit_opt}"
         return
     fi
 
-    if [ -f "${unitOpt}/sbin/unitd" ]; then
+    if [ -f "${unit_opt}/sbin/unitd" ]; then
         mkdir -p /rootfs-build/sbin /rootfs/opt
-        mv "${unitOpt}" "/rootfs-build/opt/"
-        ln -sf "${unitOpt}/sbin/unitd" /rootfs-build/sbin/unitd
+        mv "${unit_opt}" "/rootfs-build/opt/"
+        ln -sf "${unit_opt}/sbin/unitd" /rootfs-build/sbin/unitd
         return
     fi
 
-    echo "Error: Nginx Unit not found in ${unitOpt}" >&2
-    ls -al "${unitOpt}"
+    echo "Error: Nginx Unit not found in ${unit_opt}" >&2
+    ls -al "${unit_opt}"
     return 1
 }
 
